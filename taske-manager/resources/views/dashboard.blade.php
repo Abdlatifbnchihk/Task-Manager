72f7cea4ff93415b395f751334e5fdb5f359dcd4
abdellatifbencheikh43-cell <EMAIL_OF_abdellatifbencheikh43-cell>
2026-04-30 17:22:20 +0200

    install debugbar and telescop

--- taske-manager/resources/views/dashboard.blade.php
+++ taske-manager/resources/views/dashboard.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    <h1>hello</h1>
+    <h1>hello</h1>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
---
+++ taske-manager/resources/views/test.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+
+<x-slot name="header">
+    
+<h1>that my header</h1>
+</x-slot>
+
+<h1>That's my page content</h1>
+
+</x-app-layout>
